ca7e43672af20c0cc0dfb601a5a5348bfc8c41f7
reiv <reiv1992@mail.ru>
2017-03-02 16:19:34 +0400

    [+] Add const db connect

--- application/const.php
+++ application/const.php
@@ -3,3 +3,9 @@
 define( "CONTROLLER_PATH", __DIR__ . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR );    //путь к контроллерам проекта
 define( "MODEL_PATH", __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR );                //путь к моделям проекта
 define( "VIEW_PATH", __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR );                //путь к шаблонам проекта
+
+//параметры подключения к базе данных
+define( "DB_HOST", 'localhost' );
+define( "DB_NAME", 'SceletonDB' );
+define( "DB_USER", 'root' );
+define( "DB_PASSWORD", '' );
