6cdf962cf5dd4678a8d1bed7106771c655144274
reiv <reiv1992@mail.ru>
2017-03-03 12:50:36 +0400

    [+] Add method getAll()

--- application/core/Model.php
+++ application/core/Model.php
@@ -2,7 +2,50 @@
 
 namespace application\core;
 
-class Model
+use application\core\lib;
+
+abstract class Model
 {
+	static protected $table;
+	protected        $data = [ ];
+
+	public function __set( $name, $value )
+	{
+		$this->data[ $name ] = $value;
+	}
+
+	public function __get( $name )
+	{
+		return $this->data[ $name ];
+	}
+
+	public function __isset( $name )
+	{
+		return isset( $this->data[ $name ] );
+	}
+
+	/**
+	 * Получить имя таблицы данных
+	 * @return mixed - имя таблицы
+	 */
+	public static function getTableName()
+	{
+		return static::$table;
+	}
+
+	/**
+	 * Получить все записи из таблицы
+	 * @return mixed - результат запроса
+	 */
+	public static function getAll()
+	{
+		$db = new lib\DataBase();
+		$db->setClassName( get_called_class() );
+		$sql    = 'SELECT * FROM ' . static::$table . ' ORDER BY id DESC';
+		$result = $db->query( $sql );
+
+		return $result;
+	}
+
 
 }
