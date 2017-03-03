408dd1855656b61c06810c23786717ce8657c951
reiv <reiv1992@mail.ru>
2017-03-03 09:58:11 +0400

    [*] Refactoring (add db parameters)

--- application/const.php
+++ application/const.php
@@ -4,6 +4,9 @@ define( "CONTROLLER_PATH", __DIR__ . DIRECTORY_SEPARATOR . 'controllers' . DIREC
 define( "MODEL_PATH", __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR );                //путь к моделям проекта
 define( "VIEW_PATH", __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR );                //путь к шаблонам проекта
 
+
+//define( "IMAGE_PATH", __DIR__ . '..' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR );                //путь папке с изображениями
+
 //параметры подключения к базе данных
 define( "DB_HOST", 'localhost' );
 define( "DB_NAME", 'SceletonDB' );
