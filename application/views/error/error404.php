9d183132f1c40a665c444a05ee13d9e44674b4d0
reiv <reiv1992@mail.ru>
2017-03-03 09:47:15 +0400

    [+] template error404

---
+++ application/views/error/error404.php
@@ -0,0 +1,15 @@
+<html>
+<header>
+	<title>ERROR 404</title>
+</header>
+
+<body align="center">
+
+	<img src="/assets/image/error404.jpg" width="290px" height="30px"/>
+	<p>
+		<h1>УПС!!! Произошла ошибка</h1>
+	</p>
+
+</body>
+
+</html>
